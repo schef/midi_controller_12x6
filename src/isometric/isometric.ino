b2fd841de6898478faa6da56679ff7a9d57e88ff
schef <zvanstefan@gmail.com>
2017-04-28 15:46:09 +0200

    before banjo one hand

--- src/isometric/isometric.ino
+++ src/isometric/isometric.ino
@@ -8,7 +8,18 @@ int keyNote[6][12];
 int keyNoteOld[6][12];
 
 long lastDebounceTime[6][12];
-long debounceDelay = 5;
+long debounceDelay = 2;
+
+int sensorPin = A9;
+int sensorValue = 0;
+
+#define INMIN 690
+#define INMAX 1010
+#define OUTMIN 16
+#define OUTMAX 127
+
+#define VALUE_DIFERENCE 2
+#define NUMBER_OF_SAMPLES 10
 
 void initPins()
 {
@@ -49,10 +60,16 @@ int calculatePitch(int s, int d)
   return(pitch);
 }
 
-int calculateChannel(int s)
+int calculateChannel(int d)
 {
-  //s+1
-  return(1);
+  if (d <= 5)
+  {
+    return(2);
+  }
+  else
+  {
+    return(1);
+  }
 }
 
 int calculateVelocity()
@@ -64,7 +81,7 @@ void handleNoteOn(int s, int d)
 {
   if ((millis() - lastDebounceTime[s][d]) > debounceDelay)
   {
-    usbMIDI.sendNoteOn(calculatePitch(s, d), calculateVelocity(), calculateChannel(s));
+    usbMIDI.sendNoteOn(calculatePitch(s, d), calculateVelocity(), calculateChannel(d));
     keyNoteOld[s][d] = keyNote[s][d];
     lastDebounceTime[s][d] = millis();
   }
@@ -74,7 +91,7 @@ void handleNoteOff(int s, int d)
 {
   if ((millis() - lastDebounceTime[s][d]) > debounceDelay)
   {
-    usbMIDI.sendNoteOff(calculatePitch(s, d), calculateVelocity(), calculateChannel(s));
+    usbMIDI.sendNoteOff(calculatePitch(s, d), calculateVelocity(), calculateChannel(d));
     keyNoteOld[s][d] = keyNote[s][d];
     lastDebounceTime[s][d] = millis();
   }
@@ -122,14 +139,61 @@ void resposneToSwitchStatus()
   }
 }
 
+int resultArray[NUMBER_OF_SAMPLES] = {0};
+int result = 0;
+int oldResult = 0;
+
+float linearMapTo(float value, float inMin, float inMax, float outMin, float outMax)
+{
+  if (value > inMax) value = inMax;
+  if (value < inMin) value = inMin;
+  float result = (((value - inMin) * (outMax - outMin)) / (inMax - inMin)) + outMin;
+  if (result > outMax) result = outMax;
+  if (result < outMin) result = outMin;
+  //Serial.printf("liner: value: %d, result %d\r\n", (int)value, (int)result);
+  return(result);
+}
+
+int calcAverage(int *buffer)
+{
+  int result = 0;
+  for (int i = 0; i < NUMBER_OF_SAMPLES; i++)
+  {
+    result += buffer[i];
+  }
+  result = result / NUMBER_OF_SAMPLES;
+  return(result);
+}
+
 void setup()
 {                
   Serial.begin(38400);
   initPins();
 }
 
+void readAnalogValues()
+{
+  // read the value from the sensor:
+  for (int i = 0; i < NUMBER_OF_SAMPLES; i++)
+  {
+    sensorValue = analogRead(sensorPin);
+    int tempResult = linearMapTo(sensorValue, INMIN, INMAX, OUTMIN, OUTMAX);
+    //Serial.printf("value: %d, result %d\r\n", sensorValue, tempResult);
+    resultArray[i] = tempResult;
+    
+  }
+  result = calcAverage(resultArray);
+  if ((result != oldResult) && (abs(result-oldResult) > VALUE_DIFERENCE))
+  {
+    //Serial.printf("value: %d\n", result);
+    usbMIDI.sendControlChange(11, result, 1);
+    oldResult = result;
+}
+}
+
 void loop()
 {
   checkSwitchStatus();
   resposneToSwitchStatus();
+  readAnalogValues();
 }
