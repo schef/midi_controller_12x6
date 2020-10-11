7505a5acb50d0f02e5c77663f8f4d37de42daff0
schef <zvanstefan@gmail.com>
2020-10-11 19:43:14 +0200

    added drawbars

--- src/isometric/isometric.ino
+++ src/isometric/isometric.ino
@@ -103,23 +103,15 @@ public:
     return instance;
   }
 
-  void handleNote(uint8_t pitch, uint8_t channel, bool state)
+  void handleNote(uint8_t channel, uint8_t pitch, uint8_t vel)
   {
-    Serial.printf("[%u][%u]-[%u]", pitch, channel, state);
-
-    if (state)
-    {
-      usbMIDI.sendNoteOn(pitch, state * 127, channel);
-    }
-    else
-    {
-      usbMIDI.sendNoteOff(pitch, state * 127, channel);
-    }
+    Serial.printf("handleNote c[%u] p[%u] v[%u]\n", channel, pitch, vel);
+    usbMIDI.sendNoteOn(pitch, vel, channel);
   }
 
-  void handleCc(uint8_t cc, uint8_t channel, uint8_t value)
+  void handleCc(uint8_t channel, uint8_t cc, uint8_t value)
   {
-    Serial.printf("[%u][%u]-[%u]", cc, channel, value);
+    Serial.printf("handleCc c[%u] cc[%u] v[%u]\n", channel, cc, value);
     usbMIDI.sendControlChange(cc, value, channel);
   }
 };
@@ -129,89 +121,149 @@ class MidiHolder
 private:
   bool midiRequests[128] = {0};
   bool midiInField[128] = {0};
+  bool cmdRegister = false;
+
+  float linearMapTo(float value, float inMin, float inMax, float outMin, float outMax)
+  {
+    if (value > inMax)
+      value = inMax;
+    if (value < inMin)
+      value = inMin;
+    float result = (((value - inMin) * (outMax - outMin)) / (inMax - inMin)) + outMin;
+    if (result > outMax)
+      result = outMax;
+    if (result < outMin)
+      result = outMin;
+    return (result);
+  }
+
+  void checkVolume(uint8_t selectIndex, uint8_t dataIndex, bool state)
+  {
+    // selectIndex[0-5] and dataIndex[5]
+    if (dataIndex == 5 && state)
+    {
+      {
+        uint8_t volume = (uint8_t)linearMapTo(selectIndex * 1.0, 0.0, 5.0, 0.0, 127.0);
+        Serial.printf("setVolume[%u]\n", volume);
+        MidiPlayer::getInstance().handleCc(1, 7, volume);
+      }
+    }
+  }
+
+  void checkLeslie(uint8_t selectIndex, uint8_t dataIndex, bool state)
+  {
+    // selectIndex[0,1,2] and dataIndex[6]
+    if (dataIndex == 6 && state)
+    {
+      switch (selectIndex)
+      {
+      case 0:
+        Serial.printf("setLeslie[TREMOLO]\n");
+        MidiPlayer::getInstance().handleCc(1, 1, 0);
+        break;
+      case 1:
+        Serial.printf("setLeslie[OFF]\n");
+        MidiPlayer::getInstance().handleCc(1, 1, 64);
+        break;
+      case 2:
+        Serial.printf("setLeslie[CHORALE]\n");
+        MidiPlayer::getInstance().handleCc(1, 1, 127);
+        break;
+
+      default:
+        break;
+      }
+    }
+  }
+
+  void checkRegisters(uint8_t selectIndex, uint8_t dataIndex, bool state)
+  {
+    if (selectIndex == 5 && dataIndex == 6)
+    {
+      Serial.printf("cmdRegister[%u]\n", state);
+      cmdRegister = state;
+    }
+    if (cmdRegister && state)
+    {
+      // selectIndex[0-5] = value 0-127, dataIndex[0-4, 7-11] drawbars
+      uint8_t value = (uint8_t)linearMapTo(selectIndex * 1.0, 0.0, 5.0, 0.0, 127.0);
+      switch (dataIndex)
+      {
+      case 0:
+        Serial.printf("drawBar[16][%u]", value);
+        MidiPlayer::getInstance().handleCc(1, 70, value);
+        break;
+      case 1:
+        Serial.printf("drawBar[513][%u]", value);
+        MidiPlayer::getInstance().handleCc(1, 71, value);
+        break;
+      case 2:
+        Serial.printf("drawBar[8][%u]", value);
+        MidiPlayer::getInstance().handleCc(1, 72, value);
+        break;
+      case 3:
+        Serial.printf("drawBar[4][%u]", value);
+        MidiPlayer::getInstance().handleCc(1, 73, value);
+        break;
+      case 4:
+        Serial.printf("drawBar[223][%u]", value);
+        MidiPlayer::getInstance().handleCc(1, 74, value);
+        break;
+      case 7:
+        Serial.printf("drawBar[2][%u]", value);
+        MidiPlayer::getInstance().handleCc(1, 75, value);
+        break;
+      case 8:
+        Serial.printf("drawBar[135][%u]", value);
+        MidiPlayer::getInstance().handleCc(1, 76, value);
+        break;
+      case 9:
+        Serial.printf("drawBar[113][%u]", value);
+        MidiPlayer::getInstance().handleCc(1, 77, value);
+        break;
+      case 10:
+        Serial.printf("drawBar[1][%u]", value);
+        MidiPlayer::getInstance().handleCc(1, 78, value);
+        break;
+
+      default:
+        break;
+      }
+    }
+  }
+
+  void checkLowerManual(uint8_t selectIndex, uint8_t dataIndex, bool state)
+  {
+    if (dataIndex < 5)
+    {
+      //lowerKeys selectIndex[0-6], dataIndex[0-4], lowest pitch f-41
+      uint8_t midiIndex = 41 + selectIndex * 5 + dataIndex * 1;
+      Serial.printf("lowerManual[%u][%u]\n", midiIndex, state);
+      MidiPlayer::getInstance().handleNote(1, midiIndex, state * 127);
+    }
+  }
+
+  void checkUpperManual(uint8_t selectIndex, uint8_t dataIndex, bool state)
+  {
+    if (dataIndex > 6)
+    {
+      //lowerKeys selectIndex[0-6], dataIndex[7-11], lowest pitch f-65
+      uint8_t midiIndex = 65 - 7 + selectIndex * 5 + dataIndex * 1;
+      Serial.printf("upperManual[%u][%u]\n", midiIndex, state);
+      MidiPlayer::getInstance().handleNote(1, midiIndex, state * 127);
+    }
+  }
 
   void onStateChange(uint8_t selectIndex, uint8_t dataIndex, bool state)
   {
-    Serial.printf("key[%u][%u]-[%u]\n", selectIndex, dataIndex, state);
-    if (selectIndex == 5 && dataIndex == 5)
+    Serial.printf("keyIndex s[%u] d[%u] s[%u]\n", selectIndex, dataIndex, state);
+    checkVolume(selectIndex, dataIndex, state);
+    checkLeslie(selectIndex, dataIndex, state);
+    checkRegisters(selectIndex, dataIndex, state);
+    if (!cmdRegister)
     {
-      if (state) {
-        MidiPlayer::getInstance().handleCc(7, 1, 127);
-      }
-    }
-    else if (selectIndex == 5 && dataIndex == 6)
-    {
-    }
-    else if (selectIndex == 4 && dataIndex == 5)
-    {
-      if (state) {
-        MidiPlayer::getInstance().handleCc(7, 1, 106);
-      }
-    }
-    else if (selectIndex == 4 && dataIndex == 6)
-    {
-    }
-    else if (selectIndex == 3 && dataIndex == 5)
-    {
-      if (state) {
-        MidiPlayer::getInstance().handleCc(7, 1, 85);
-      }
-    }
-    else if (selectIndex == 3 && dataIndex == 6)
-    {
-    }
-    else if (selectIndex == 2 && dataIndex == 5)
-    {
-      if (state) {
-        MidiPlayer::getInstance().handleCc(7, 1, 64);
-      }
-    }
-    else if (selectIndex == 2 && dataIndex == 6)
-    {
-      if (state) {
-        MidiPlayer::getInstance().handleCc(1, 1, 0);
-      }
-    }
-    else if (selectIndex == 1 && dataIndex == 5)
-    {
-      if (state) {
-        MidiPlayer::getInstance().handleCc(7, 1, 43);
-      }
-    }
-    else if (selectIndex == 1 && dataIndex == 6)
-    {
-      if (state) {
-        MidiPlayer::getInstance().handleCc(1, 1, 127);
-      }
-    }
-    else if (selectIndex == 0 && dataIndex == 5)
-    {
-      if (state) {
-        MidiPlayer::getInstance().handleCc(7, 1, 22);
-      }
-    }
-    else if (selectIndex == 0 && dataIndex == 6)
-    {
-      if (state) {
-        MidiPlayer::getInstance().handleCc(1, 1, 64);
-      }
-    }
-    else
-    {
-      if (dataIndex < 5)
-      {
-        //lowerKeys selectIndex[0-6], dataIndex[0-4], midiStart f[41]
-        uint8_t midiIndex = 29 + selectIndex * 5 + dataIndex * 1;
-        Serial.printf("midi[%lu]\n", midiIndex);
-        MidiPlayer::getInstance().handleNote(midiIndex, 2, state);
-      }
-      else if (dataIndex > 6)
-      {
-        //lowerKeys selectIndex[0-6], dataIndex[7-11], midiStart f[65]
-        uint8_t midiIndex = 53 - 7 + selectIndex * 5 + dataIndex * 1;
-        Serial.printf("midi[%lu]\n", midiIndex);
-        MidiPlayer::getInstance().handleNote(midiIndex, 1, state);
-      }
+      checkLowerManual(selectIndex, dataIndex, state);
+      checkUpperManual(selectIndex, dataIndex, state);
     }
   }
 
