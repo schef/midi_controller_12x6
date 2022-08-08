0a5a2bcab675bd0f154991b88ae77a95e10e76c9
schef <zvanstefan@gmail.com>
2022-08-08 12:45:47 +0200

    added hammond stuff and program change

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,6 +3,12 @@ import midi_player
 import common
 
 CHANNEL = 1
+HAMMOND_VOLUME_CC = 35
+HAMMOND_EXPRESSION_CC = 2
+HAMMOND_LESLIE_SPEED_CC = 1
+HAMMOND_LESLIE_BREAK_CC = 93
+
+program_change_index = 0
 
 
 def get_button_midi_num(select_index, data_index):
@@ -16,27 +22,33 @@ def get_button_midi_num(select_index, data_index):
     return midi_index
 
 
-def get_pot_midi_num(pot_index, state):
-    if pot_index == 0:
-        return 1, state
-    elif pot_index == 1:
-        return 71, state
-    return -1
-
-
 def on_button_change(select_index, data_index, state):
+    global program_change_index
     midi_index = get_button_midi_num(select_index, data_index)
     if midi_index != -1:
         if state:
             midi_player.note_on(CHANNEL, midi_index, 100)
         else:
             midi_player.note_off(CHANNEL, midi_index)
+    else:
+        if state:
+            if data_index == 5:
+                if program_change_index > 0:
+                    program_change_index -= 1
+                midi_player.program_change(CHANNEL, program_change_index)
+            elif data_index == 6:
+                if program_change_index < 3:
+                    program_change_index += 1
+                midi_player.program_change(CHANNEL, program_change_index)
 
 
 def on_pot_change(index, state):
-    cc, new_state = get_pot_midi_num(index, state)
-    if cc != -1:
-        midi_player.cc_message(CHANNEL, cc, new_state)
+    if index == 0:
+        midi_player.cc_message(CHANNEL, HAMMOND_VOLUME_CC, state)
+    elif index == 1:
+        midi_player.cc_message(CHANNEL, HAMMOND_LESLIE_SPEED_CC, state)
+        if state > 64 - 10 and state < 64 + 10:
+            midi_player.cc_message(CHANNEL, HAMMOND_LESLIE_BREAK_CC, state)
 
 
 def init():
